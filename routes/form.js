07e186c2cc0744bd44dfa94ab88fbb21514a7165
IlyaGureshidze <enigmaster90@gmail.com>
2020-12-12 21:21:43 +0300

    fix comment

--- routes/form.js
+++ routes/form.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET form. */
 router.get('/', function(req, res, next) {
   res.render('form', {title: 'Новое сообщение'});
 });
